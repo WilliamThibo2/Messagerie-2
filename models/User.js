4cc36f85ee7b49a09a799d28e69b7f7d5d3283ff
WilliamThibo2 <williamthibault@hotmail.fr>
2024-11-21 19:51:33 -0500

    Add files via upload

--- models/User.js
+++ models/User.js
@@ -1,1 +1,9 @@
+const mongoose = require('mongoose');
 
+const UserSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String }, // Peut être facultatif pour les utilisateurs Google
+    googleId: { type: String, unique: true }, // Ajout de googleId
+});
+
+module.exports = mongoose.model('User', UserSchema);
