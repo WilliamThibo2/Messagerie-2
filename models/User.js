eeeacb236bee19a37336d6d5e3b3dff19e2f38c7
WilliamThibo2 <williamthibault@hotmail.fr>
2024-11-23 10:49:27 -0500

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,9 +1,22 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    email: { type: String, required: true, unique: true },
-    password: { type: String }, // Peut être facultatif pour les utilisateurs Google
-    googleId: { type: String, unique: true }, // Ajout de googleId
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    password: { 
+        type: String 
+    }, // Peut être facultatif pour les utilisateurs Google
+    googleId: { 
+        type: String, 
+        unique: true,
+        sparse: true 
+    },
+    username: {
+        type: String
+    }
 });
 
 module.exports = mongoose.model('User', UserSchema);
