7a664301a389c8b8313b715c49f3e8c3473b5cd7
WilliamThibo2 <williamthibault@hotmail.fr>
2024-11-23 10:55:05 -0500

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,41 +1,56 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/User');
+require('dotenv').config();
 
 module.exports = function (passport) {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: 'https://messagerie2-1.onrender.com/auth/google/callback'
-    },
-    async (accessToken, refreshToken, profile, done) => {
+        callbackURL: process.env.GOOGLE_CALLBACK_URL,
+        passReqToCallback: true
+    }, async (request, accessToken, refreshToken, profile, done) => {
         try {
             console.log("Google profile:", profile);
-            let user = await User.findOne({ googleId: profile.id });
+            
+            let user = await User.findOne({ 
+                $or: [
+                    { googleId: profile.id },
+                    { email: profile.emails[0].value }
+                ]
+            });
 
             if (!user) {
-                user = await User.create({
+                user = new User({
                     googleId: profile.id,
                     email: profile.emails[0].value,
-                    username: profile.displayName
+                    username: profile.displayName || profile.emails[0].value.split('@')[0]
                 });
-                console.log("User created:", user);
+                await user.save();
+            } else {
+                // Mettre à jour l'ID Google si nécessaire
+                if (!user.googleId) {
+                    user.googleId = profile.id;
+                    await user.save();
+                }
             }
+
             return done(null, user);
         } catch (error) {
-            console.error("Error during authentication:", error);
+            console.error("Erreur d'authentification Google:", error);
             return done(error, null);
         }
     }));
 
-    passport.serializeUser((user, done) => done(null, user.id));
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    });
+
     passport.deserializeUser(async (id, done) => {
         try {
             const user = await User.findById(id);
-            console.log("Deserialized user:", user);
             done(null, user);
         } catch (error) {
-            console.error("Error deserializing user:", error);
             done(error, null);
         }
     });
