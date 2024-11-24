4fda0297523549f26ea4bde8eb3c6c2236cc2688
WilliamThibo2 <williamthibault@hotmail.fr>
2024-11-24 11:49:41 -0500

    Update messageRoutes.js

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,17 +1,15 @@
 const express = require('express');
-const { createOrGetConversation, getConversationMessages } = require('../controllers/conversationController');
-const { sendMessage } = require('../controllers/messageController');
+const {
+    sendMessage,
+    getMessages,
+    deleteMessage
+} = require('../controllers/messageController');
 const authenticate = require('../middlewares/authenticate');
 
 const router = express.Router();
 
-// Route pour créer ou récupérer une conversation
-router.post('/conversation', authenticate, createOrGetConversation);
-
-// Route pour récupérer les messages d'une conversation
-router.get('/conversation/:conversationId/messages', authenticate, getConversationMessages);
-
-// Route pour envoyer un message (mise à jour pour inclure la conversation)
 router.post('/send', authenticate, sendMessage);
+router.get('/messages/:toEmail', authenticate, getMessages);
+router.delete('/message/:messageId', authenticate, deleteMessage);
 
 module.exports = router;
